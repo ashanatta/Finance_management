8e0908cec4dc65caf3618418f36cd9ee1e2bf9cb
ashanatta <142013731+ashanatta@users.noreply.github.com>
2025-06-14 23:32:22 +0500

    ashan commit

---
+++ resources/views/income/listing.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>listing</title>
+</head>
+<body>
+    <form action="" method="post">
+        <label for="name">name</label>
+        <input type="text">
+        <label for="price">price</label>
+        <input type="text">
+    </form>
+</body>
+</html>
