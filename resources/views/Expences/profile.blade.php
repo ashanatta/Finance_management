e94b67f2a807fd01120eb7f564d7706589e8acbf
ashanatta <142013731+ashanatta@users.noreply.github.com>
2025-06-15 00:00:12 +0500

    ashanadd profile

---
+++ resources/views/Expences/profile.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>profile</title>
+</head>
+<body>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>new profile</title>
+    </head>
+    <body>
+        
+    </body>
+    </html>
+</body>
+</html>
