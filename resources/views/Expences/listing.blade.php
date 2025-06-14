44cb7ba5246aa27b58d0cd50adeb6b62df261e4a
ashanatta <142013731+ashanatta@users.noreply.github.com>
2025-06-14 23:52:57 +0500

    ali commit

---
+++ resources/views/Expences/listing.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Ecxpences</title>
+</head>
+<body>
+    <table>
+        <tbody>
+            <td>ashan</td>
+            <td>main</td>
+            <td>ali</td>
+        </tbody>
+    </table>
+</body>
+</html>
