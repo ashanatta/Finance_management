d16d0a05953bef03775bdb1dbe3abb7640acdd16
ashanatta <142013731+ashanatta@users.noreply.github.com>
2025-06-15 11:34:07 +0500

    conflict for ali chages

--- resources/views/income/listing.blade.php
+++ resources/views/income/listing.blade.php
@@ -8,7 +8,10 @@
 </head>
 <body>
     <form action="" method="post">
-        <label for="name">name</label>
+        <label for="name">welcome to git</label>
+        <input type="text">
+        <input type="text">
+        <input type="text">
         <input type="text">
         <label for="price">ali </label>
         <input type="text">
